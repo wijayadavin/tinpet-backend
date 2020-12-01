20d0047eb346ec7996351ba946c8ef80911db8f3
Davin Wijaya <wijayadavin@gmail.com>
2020-12-01 19:38:05 +0700

    register add + for phone number

--- routes/auth/registerRoute.js
+++ routes/auth/registerRoute.js
@@ -5,6 +5,7 @@ const UserController = require('../../controller/userController')
 
 app.post('/auth/register', async (req, res, next) => {
   try {
+    req.body.mobileNumber = "+" + req.body.mobileNumber.replace(" ", "")
     const result = await new UserController(req.body)
       .register()
     res.send(result)
