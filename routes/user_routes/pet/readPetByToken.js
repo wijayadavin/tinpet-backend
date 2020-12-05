704c295d012f61d529b5fb6d563008293cc337a3
Davin Wijaya <wijayadavin@gmail.com>
2020-12-05 15:56:01 +0700

    get pet by token feature

---
+++ routes/user_routes/pet/readPetByToken.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express.Router()
+const routeErrorHandler = require('../../../middleware/errorHandler')
+const { petResultParser } = require('../../../helper/modelHelpers/pet')
+const PetController = require('../../../controller/petController')
+const auth = require('../../../middleware/auth')
+
+
+// get all or by query:
+app.get('/profile/pet',
+    auth.authenticate('bearer', { session: false }),
+    async (req, res, next) => {
+        try {
+            const result = await new PetController({ userId: req.user.id })
+                .filter()
+
+            // output & data preprocessing:
+            res.send(result.map(petResultParser))
+        } catch (err) { next(err) }
+    })
+
+
+app.use(routeErrorHandler)
+
+module.exports = app
