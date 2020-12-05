a53b40eeaf39e861e5fef0ae98765450cd557d12
Davin Wijaya <wijayadavin@gmail.com>
2020-12-05 15:30:24 +0700

    root

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -3,6 +3,8 @@ const app = express.Router()
 const db = require('../controller/dbController')
 
 app.get('/', async (req, res) => {
-  res.send('Hello World!')
+  res.send(`Welcome to TinPet API!
+  Our API Documentation:
+  <a href=https://documenter.getpostman.com/view/13619777/TVmFkgGw#055d1fd9-fa7d-4145-93de-71a5daf16893>click here</a>`)
 })
 module.exports = app
