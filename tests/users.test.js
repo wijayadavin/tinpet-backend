5398c0b3ce1e5e7e65df36b51da7374b31cd63b3
Davin Wijaya <wijayadavin@gmail.com>
2020-12-12 17:36:20 +0700

    test user patch

--- tests/users.test.js
+++ tests/users.test.js
@@ -5,7 +5,7 @@ const chaiHttp = require('chai-http')
 const chai = require('chai')
 const server = require('../server')
 const Users = require("../models/Users")
-const { delay } = require("lodash")
+const { checkPassword } = require("../helper/bcryptHelper");
 const should = chai.should()
 const createdID = []
 chai.use(chaiHttp)
@@ -70,34 +70,6 @@ describe(`========= USERS =========`, () => {
                 })
         })
     })
-    describe('GET /profile', () => {
-        it('it should NOT be able to get user profile since no token was sent', (done) => {
-            chai
-                .request(server)
-                .get('/profile')
-                .end((err, res) => {
-                    res.should.have.status(401)
-                    done()
-                })
-        })
-        it(`it should GET the user's profile by token`, (done) => {
-            chai
-                .request(server)
-                .get('/profile')
-                .set('Authorization', `Bearer ${userToken}`)
-                .end((err, res) => {
-                    res.should.have.status(200)
-                    res.body.should.be.an('object')
-                    res.body.should.include.keys(
-                        'id', 'name', 'email', 'password',
-                        'mobileNumber', 'address', 'city',
-                        'userImage', 'createdAt', 'updatedAt')
-                    res.body.userImage.should.include.keys(
-                        'url')
-                    done()
-                })
-        })
-    })
     describe('GET /user', () => {
         it('it should get all users data', done => {
             chai
@@ -129,6 +101,82 @@ describe(`========= USERS =========`, () => {
                 .get(`/user/${random.uuid()}`)
                 .end((err, res) => {
                     res.should.have.status(404)
+                    done()
+                })
+        })
+    })
+    describe('GET /profile', () => {
+        it('it should NOT be able to get user profile since no token was sent', (done) => {
+            chai
+                .request(server)
+                .get('/profile')
+                .end((err, res) => {
+                    res.should.have.status(401)
+                    done()
+                })
+        })
+        it(`it should GET the user's profile by token`, (done) => {
+            chai
+                .request(server)
+                .get('/profile')
+                .set('Authorization', `Bearer ${userToken}`)
+                .end((err, res) => {
+                    res.should.have.status(200)
+                    res.body.should.be.an('object')
+                    res.body.should.include.keys(
+                        'id', 'name', 'email', 'password',
+                        'mobileNumber', 'address', 'city',
+                        'userImage', 'createdAt', 'updatedAt')
+                    res.body.userImage.should.include.keys(
+                        'url')
+                    done()
+                })
+        })
+    })
+    describe('PATCH /profile', () => {
+        it('it should NOT be able to patch user profile since no token was sent', (done) => {
+            chai
+                .request(server)
+                .patch('/profile')
+                .send({
+                    name: name.firstName() + name.lastName(),
+                    email: internet.email(),
+                    password: random.alphaNumeric(9),
+                    mobileNumber: '08123456789',
+                    address: address.streetAddress(),
+                    city: 'Bandung',
+                })
+                .end((err, res) => {
+                    res.should.have.status(401)
+                    done()
+                })
+        })
+        it(`it should EDIT the user's profile by token`, (done) => {
+            const id = createdID.slice(-1).pop()
+            const newUserBody = {
+                name: 'editedName',
+                email: 'editedEmail@email.com',
+                password: 'editedPassword123',
+                mobileNumber: '08987654321',
+                address: 'editedStreetAddress',
+                city: 'Bandung',
+            }
+            chai
+                .request(server)
+                .patch('/profile')
+                .set('Authorization', `Bearer ${userToken}`)
+                .send(newUserBody)
+                .end((err, res) => {
+                    res.body.password = checkPassword(res.body.password, newUserBody.password)
+                    res.should.have.status(200)
+                    res.body.should.be.an('object')
+                    res.body.should.have.property('id').eql(id)
+                    res.body.should.have.property('name').eql(newUserBody.name)
+                    res.body.should.have.property('email').eql(newUserBody.email)
+                    res.body.should.have.property('password').eql(true)
+                    res.body.should.have.property('mobileNumber').eql(newUserBody.mobileNumber)
+                    res.body.should.have.property('address').eql(newUserBody.address)
+                    res.body.should.have.property('city').eql(newUserBody.city)
                     done()
                 })
         })
